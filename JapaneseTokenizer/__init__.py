54bfdcf9249edfdb03a1a668d1c93ed3bbc758dd
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2016-03-05 02:01:59 +0900

    fixed installing issues

--- JapaneseTokenizer/__init__.py
+++ JapaneseTokenizer/__init__.py
@@ -1,6 +1,3 @@
-__author__ = 'kensuke-mi'
-__version__ = 0.5
-
 from mecab_wrapper import MecabWrapper
 from JapaneseTokenizer.datamodels import TokenizedSenetence
 from JapaneseTokenizer.datamodels import FilteredObject
