07d9be93a2987a470e69f5e1388869a6a15d379a
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2016-12-28 09:06:33 +0900

    completed jumanpp interface for python3

--- JapaneseTokenizer/datamodels.py
+++ JapaneseTokenizer/datamodels.py
@@ -2,6 +2,7 @@
 from MeCab import Node
 from typing import List, Union, Any, Tuple, Dict
 from future.utils import string_types, text_type
+from JapaneseTokenizer.common import filter
 import sys
 __author__ = 'kensuke-mi'
 
@@ -11,7 +12,7 @@ python_version = sys.version_info
 class TokenizedResult(object):
     def __init__(self, node_obj, tuple_pos, word_stem, word_surface,
                  is_feature=True, is_surface=False, misc_info=None, analyzed_line=None):
-        # type: (Union[Node, None], Union[str, Tuple[Any], str, str, bool, bool, Union[None, Dict[str, Any]], str])->None
+        # type: (Union[Node, None], Union[str, Tuple[...], str, str, bool, bool, Union[None, Dict[str, Any]], str])->None
         assert isinstance(node_obj, (Node, type(None)))
         assert isinstance(tuple_pos, (str, string_types, tuple))
         assert isinstance(word_stem, (str, string_types))
@@ -71,9 +72,63 @@ class TokenizedSenetence(object):
 
         return sentence_in_list_obj
 
+    def __convert_str(self, p_c_tuple):
+        # type: (Tuple[...]) -> List[str]
+        converted = []
+        for item in p_c_tuple:
+            if isinstance(item, str):
+                converted.append(item)
+            else:
+                converted.append(item)
+        return converted
+
+    def __check_pos_condition_str(self, pos_condistion):
+        # type: (List[Tuple[...]]) -> List[Tuple[...]]
+        """* What you can do
+        - Check your pos condition is correct or NOT
+
+        * Input
+        - pos_condistion
+            - List of Tuple which has POS element to keep.
+            - Keep in your mind, each tokenizer has different POS structure.
+            >>> [('名詞', '固有名詞'), ('動詞', )]
+        """
+        assert isinstance(pos_condistion, list)
+        # [ ('', '', '') ]
+
+        return [
+            tuple(self.__convert_str(p_c_tuple))
+            for p_c_tuple
+            in pos_condistion]
+
+    def filter(self, pos_condition=None, stopwords=None):
+        # type: (List[Any], List[str]) ->  FilteredObject
+        assert isinstance(pos_condition, (type(None), list))
+        assert isinstance(stopwords, (type(None), list))
+
+        if isinstance(stopwords, type(None)):
+            s_words = []
+        else:
+            s_words = stopwords
+
+        if isinstance(pos_condition, type(None)):
+            p_condition = []
+        else:
+            p_condition = self.__check_pos_condition_str(pos_condition)
+
+        filtered_object = filter.filter_words(
+            tokenized_obj=self,
+            valid_pos=p_condition,
+            stopwords=s_words
+        )
+        assert isinstance(filtered_object, FilteredObject)
+
+        return filtered_object
+
+
 class FilteredObject(TokenizedSenetence):
     def __init__(self, sentence, tokenized_objects, pos_condition, stopwords):
-        # type: (str, TokenizedResult, List[Any], List[str]) -> None
+        # type: (str, List[TokenizedResult], List[...], List[str]) -> None
         super(FilteredObject, self).__init__(
             sentence=sentence,
             tokenized_objects=tokenized_objects
