71472426a319ada7eea5a62dbe4dd09e3837ef21
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2017-01-13 09:29:10 +0900

    version 1.2.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if python_version >= (3, 0, 0):
 else:
     install_requires = ['pypandoc', 'future', 'six', 'mecab-python', 'jaconv>=0.2', 'pyknp', 'kytea', 'pip>=8.1.0', 'typing']
 
-version = '1.2.6'
+version = '1.2.7'
 name = 'JapaneseTokenizer'
 short_description = '`JapaneseTokenizer` is a package for easy Japanese Tokenization'
 
