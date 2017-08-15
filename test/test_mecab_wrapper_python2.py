14b3f3f2f40203334085e9b5f37e6f63e6676034
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2017-08-15 11:16:47 +0900

    added a test case for when un-expected field appear in string from mecab-neologd result

--- test/test_mecab_wrapper_python2.py
+++ test/test_mecab_wrapper_python2.py
@@ -4,6 +4,8 @@ __author__ = 'kensuke-mi'
 import sys
 import unittest
 from JapaneseTokenizer.mecab_wrapper.mecab_wrapper_python2 import MecabWrapper
+from JapaneseTokenizer.datamodels import TokenizedSenetence
+from six import string_types
 import os
 python_version = sys.version_info
 
@@ -11,32 +13,45 @@ python_version = sys.version_info
 class TestMecabWrapperPython2(unittest.TestCase):
     def setUp(self):
         self.test_senetence = u'紗倉 まな（さくらまな、1993年3月23日 - ）は、日本のAV女優。'
+        self.test_sentence2 = u'午前零時。午前3時。3時。'
         self.path_user_dict = os.path.join(os.path.dirname(__file__), 'resources/test/userdict.csv')
 
+    def test_neologd_parse(self):
+        """* Test case
+        - neologd辞書で正しく分割できることを確認する
+        """
+        mecab_obj = MecabWrapper(dictType='neologd')
+        parsed_obj = mecab_obj.tokenize(sentence=self.test_senetence)
+        self.assertTrue(parsed_obj, TokenizedSenetence)
+        self.assertTrue(isinstance(parsed_obj.convert_list_object(), list))
+        self.assertTrue(all(isinstance(mrph, string_types) for mrph in parsed_obj.convert_list_object()))
 
-    def test_init_mecab_wrapper(self):
-        dictType = "neologd"
-        mecab_obj = MecabWrapper(dictType=dictType)
-        assert isinstance(mecab_obj, MecabWrapper)
-
-        return mecab_obj
-
+        parsed_obj = mecab_obj.tokenize(sentence=self.test_sentence2)
+        self.assertTrue(parsed_obj, TokenizedSenetence)
+        self.assertTrue(isinstance(parsed_obj.convert_list_object(), list))
+        self.assertTrue(all(isinstance(mrph, string_types) for mrph in parsed_obj.convert_list_object()))
 
     def test_default_parse(self):
-        mecab_obj = self.test_init_mecab_wrapper()
+        """* Test case
+        - デフォルトの状態で動作を確認する
+        """
+        dictType = "ipadic"
+        mecab_obj = MecabWrapper(dictType=dictType)
+        assert isinstance(mecab_obj, MecabWrapper)
         parsed_obj = mecab_obj.tokenize(sentence=self.test_senetence, return_list=True)
         assert isinstance(parsed_obj, list)
         if python_version >= (3, 0, 0):
             for morph in parsed_obj: assert isinstance(morph, str)
             print(parsed_obj)
         else:
-            for morph in parsed_obj: assert isinstance(morph, unicode)
+            for morph in parsed_obj: assert isinstance(morph, string_types)
 
 
     def test_init_userdict(self):
-        dictType = "user"
-
-        mecab_obj = MecabWrapper(dictType=dictType, pathUserDictCsv=self.path_user_dict)
+        """* Test case
+        - すべての辞書を利用した場合の動作を確認する
+        """
+        mecab_obj = MecabWrapper(dictType='all', pathUserDictCsv=self.path_user_dict)
         assert isinstance(mecab_obj, MecabWrapper)
 
         res = mecab_obj.tokenize(sentence=self.test_senetence, return_list=True)
